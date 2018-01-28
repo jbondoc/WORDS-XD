c02901a29dc2e773a19d8708800685c17f81652a
Sam <samtrinidad@gmail.com>
2018-01-27 22:12:47 -0600

    added some front

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,30 @@
-console.log("helllo");
+
+
+
+
+
+document.getElementById('new-letters').addEventListener('click', function(e) {
+    let letters = document.getElementsByClassName('letters')[0];
+    letters.innerHTML = '';
+    let letter_array = makeLetters();
+
+    letter_array.forEach( function (element) {
+        letters.appendChild(element);
+    });
+});
+
+function makeLetters() {
+    let text = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let letters = new Array(7);
+
+    for (let i = 0; i < letters.length ; i++) {
+        let div = document.createElement('div');
+        div.className = 'letter-block';
+        div.innerHTML = possible.charAt(Math.floor(Math.random() * possible.length));
+        console.log(div);
+        letters[i] = div;
+    }
+   console.log(letters);
+    return letters;
+}
